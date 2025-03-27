72f2bd0935e06ca8ceab719cd990a7fd2d73221b
Francisco Ruiz <franciscoruiz@socomti.mx>
2025-03-27 02:54:18 -0600

    Agregado setup_mariadb.sh con passwords actualizados

---
+++ provisioning/scripts/setup_mariadb.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# SOCOTI - Instalacion de MariaDB para infraestructura modular
+# CT130 - Solo acceso interno, sin root remoto
+
+echo "[SOCOTI] Instalando MariaDB..."
+apt update && apt install -y mariadb-server
+
+echo "[SOCOTI] Configurando seguridad de MariaDB..."
+sed -i 's/^bind-address\s*=.*/bind-address = 10.0.0.130/' /etc/mysql/mariadb.conf.d/50-server.cnf
+
+echo "[SOCOTI] Reiniciando MariaDB..."
+systemctl restart mariadb
+
+echo "[SOCOTI] Protegiendo root..."
+mysql -e "UPDATE mysql.user SET Password=PASSWORD('.T3petong0*'), plugin='mysql_native_password' WHERE User='root';"
+mysql -e "DELETE FROM mysql.user WHERE User='';"
+mysql -e "DROP DATABASE IF EXISTS test;"
+mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+mysql -e "FLUSH PRIVILEGES;"
+
+echo "[SOCOTI] Creando base de datos y usuario para Nextcloud..."
+mysql -e "CREATE DATABASE nextcloud;"
+mysql -e "CREATE USER 'nextcloud'@'10.0.0.%' IDENTIFIED BY '.T3petong0*';"
+mysql -e "GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'10.0.0.%';"
+
+echo "[SOCOTI] Creando base de datos y usuario para SnipeIT..."
+mysql -e "CREATE DATABASE snipeit;"
+mysql -e "CREATE USER 'snipeit'@'10.0.0.%' IDENTIFIED BY '.T3petong0*';"
+mysql -e "GRANT ALL PRIVILEGES ON snipeit.* TO 'snipeit'@'10.0.0.%';"
+
+echo "[SOCOTI] Todo listo. MariaDB funcionando solo en red interna."
