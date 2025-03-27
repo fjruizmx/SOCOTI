062e116147b2ed0ef1a55e6fa2d78bfcd7bf0744
Francisco Ruiz <franciscoruiz@socomti.mx>
2025-03-27 03:16:30 -0600

    Fixed

--- provisioning/scripts/setup_mariadb.sh
+++ provisioning/scripts/setup_mariadb.sh
@@ -3,7 +3,7 @@
 # SOCOTI - Instalacion de MariaDB para infraestructura modular
 # CT130 - Solo acceso interno, sin root remoto
 
-ROOTPASS=".T3petong0*"
+DB_ROOT_PASS=".T3petong0*"
 
 echo "[SOCOTI] Instalando MariaDB..."
 apt update && apt install -y mariadb-server
@@ -15,20 +15,24 @@ echo "[SOCOTI] Reiniciando MariaDB..."
 systemctl restart mariadb
 
 echo "[SOCOTI] Protegiendo root..."
-mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${ROOTPASS}';"
-mysql -u root -p"${ROOTPASS}" -e "DELETE FROM mysql.user WHERE User='';"
-mysql -u root -p"${ROOTPASS}" -e "DROP DATABASE IF EXISTS test;"
-mysql -u root -p"${ROOTPASS}" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
-mysql -u root -p"${ROOTPASS}" -e "FLUSH PRIVILEGES;"
+mysql -uroot -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '$DB_ROOT_PASS';"
+mysql -uroot -p"$DB_ROOT_PASS" -e "DELETE FROM mysql.user WHERE User='';"
+mysql -uroot -p"$DB_ROOT_PASS" -e "DROP DATABASE IF EXISTS test;"
+mysql -uroot -p"$DB_ROOT_PASS" -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
+mysql -uroot -p"$DB_ROOT_PASS" -e "FLUSH PRIVILEGES;"
 
 echo "[SOCOTI] Creando base de datos y usuario para Nextcloud..."
-mysql -u root -p"${ROOTPASS}" -e "CREATE DATABASE nextcloud;"
-mysql -u root -p"${ROOTPASS}" -e "CREATE USER 'nextcloud'@'10.0.0.%' IDENTIFIED BY '${ROOTPASS}';"
-mysql -u root -p"${ROOTPASS}" -e "GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'10.0.0.%';"
+mysql -uroot -p"$DB_ROOT_PASS" -e "DROP DATABASE IF EXISTS nextcloud;"
+mysql -uroot -p"$DB_ROOT_PASS" -e "DROP USER IF EXISTS 'nextcloud'@'10.0.0.%';"
+mysql -uroot -p"$DB_ROOT_PASS" -e "CREATE DATABASE nextcloud;"
+mysql -uroot -p"$DB_ROOT_PASS" -e "CREATE USER 'nextcloud'@'10.0.0.%' IDENTIFIED BY '$DB_ROOT_PASS';"
+mysql -uroot -p"$DB_ROOT_PASS" -e "GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'10.0.0.%';"
 
 echo "[SOCOTI] Creando base de datos y usuario para SnipeIT..."
-mysql -u root -p"${ROOTPASS}" -e "CREATE DATABASE snipeit;"
-mysql -u root -p"${ROOTPASS}" -e "CREATE USER 'snipeit'@'10.0.0.%' IDENTIFIED BY '${ROOTPASS}';"
-mysql -u root -p"${ROOTPASS}" -e "GRANT ALL PRIVILEGES ON snipeit.* TO 'snipeit'@'10.0.0.%';"
+mysql -uroot -p"$DB_ROOT_PASS" -e "DROP DATABASE IF EXISTS snipeit;"
+mysql -uroot -p"$DB_ROOT_PASS" -e "DROP USER IF EXISTS 'snipeit'@'10.0.0.%';"
+mysql -uroot -p"$DB_ROOT_PASS" -e "CREATE DATABASE snipeit;"
+mysql -uroot -p"$DB_ROOT_PASS" -e "CREATE USER 'snipeit'@'10.0.0.%' IDENTIFIED BY '$DB_ROOT_PASS';"
+mysql -uroot -p"$DB_ROOT_PASS" -e "GRANT ALL PRIVILEGES ON snipeit.* TO 'snipeit'@'10.0.0.%';"
 
 echo "[SOCOTI] Todo listo. MariaDB funcionando solo en red interna."
