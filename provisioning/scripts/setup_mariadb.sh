82b07e6b3125f159d5e3f5d180134bf800e24d5a
Francisco Ruiz <franciscoruiz@socomti.mx>
2025-03-27 03:01:28 -0600

    Fix: update de contraseña root con ALTER USER en setup_mariadb.sh

--- provisioning/scripts/setup_mariadb.sh
+++ provisioning/scripts/setup_mariadb.sh
@@ -13,7 +13,7 @@ echo "[SOCOTI] Reiniciando MariaDB..."
 systemctl restart mariadb
 
 echo "[SOCOTI] Protegiendo root..."
-mysql -e "UPDATE mysql.user SET Password=PASSWORD('.T3petong0*'), plugin='mysql_native_password' WHERE User='root';"
+mysql -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '.T3petong0*';"
 mysql -e "DELETE FROM mysql.user WHERE User='';"
 mysql -e "DROP DATABASE IF EXISTS test;"
 mysql -e "DELETE FROM mysql.db WHERE Db='test' OR Db='test\\_%';"
@@ -26,7 +26,4 @@ mysql -e "GRANT ALL PRIVILEGES ON nextcloud.* TO 'nextcloud'@'10.0.0.%';"
 
 echo "[SOCOTI] Creando base de datos y usuario para SnipeIT..."
 mysql -e "CREATE DATABASE snipeit;"
-mysql -e "CREATE USER 'snipeit'@'10.0.0.%' IDENTIFIED BY '.T3petong0*';"
-mysql -e "GRANT ALL PRIVILEGES ON snipeit.* TO 'snipeit'@'10.0.0.%';"
-
-echo "[SOCOTI] Todo listo. MariaDB funcionando solo en red interna."
+mysql -e
